a788d94667a58af95b8a17c0afe7a77cfaf6143f
titoeb <timtoebrock@googlemail.com>
2022-01-26 09:32:21 +0100

    Added unnecessary comment

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
 
-
+# Creating web-app
 app = FastAPI(title="Predicting Wine Class with batching")
 
 # Open classifier in global scope
